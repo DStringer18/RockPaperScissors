561d2a7e054fc379c3920567868dd55fa45c1ab1
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-06-06 18:33:04 +0200

    first attempt to fix broken js

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,53 +1,66 @@
 let humanChoice
 
 const roundNumberDisplay = document.getElementById('round-number');
-
-const computerChoiceDisplay = document.getElementById('computer-choice');
 const humanScoreDisplay = document.getElementById('human-score');
 const computerScoreDisplay = document.getElementById('computer-score');
-
-const computerWinsDisplay = document.getElementById('computer-wins');
-
-const rockButton = document.getElementById('h_rock')
-const paperButton = document.getElementById('h_paper')
-const scissorsButton = document.getElementById('h_scissors')
-
+const resultsDisplay = document.getElementById('results');
+const rockButton = document.getElementById('h_rock');
+const paperButton = document.getElementById('h_paper');
+const scissorsButton = document.getElementById('h_scissors');
 const playButton = document.getElementById('play');
 const nextRoundButton = document.getElementById('next-round');
+console.log("test");
 
 rockButton.addEventListener('click', () => {
     humanChoice = "rock"
-})
+});
 paperButton.addEventListener('click', () => {
     humanChoice = "paper"
-})
+});
 scissorsButton.addEventListener('click', () => {
     humanChoice = "scissors"
-})
+});
 
 
 
 playButton.addEventListener('click', () => {
-    // Make a random 'computer choice' and display
+    // Make a random 'computer choice' and display:
     const computerChoice = Math.random();
     if (computerChoice <= 1/3) {
-        computerChoice = "rock";
+        computerChoice = "rock"
         document.getElementById('c_default').style.display = "none"
         document.getElementById('c_rock').style.display = "block"
       } else if (computer < 2/3) {
-        computerChoice = "scissors";
+        computerChoice = "scissors"
         document.getElementById('c_default').style.display = "none"
         document.getElementById('c_scissors').style.display = "block"
       } else {
-        computerChoice = "paper";
+        computerChoice = "paper"
         document.getElementById('c_default').style.display = "none"
         document.getElementById('c_paper').style.display = "block"
-      }
-    // Compare Choices
-    compareChoices(humanChoice, computerChoice);
-    
+      };
+    // Determine if the human or computer wins and display:
+    const whoIsWinner = compareChoices(humanChoice, computerChoice);
+    resultsDisplay.innerText = whoIsWinner;
 
-})
+    // Display the current scores:
+    humanScoreDisplay.innerText = humanScore;
+    computerScoreDisplay.innerText = computerScore;
+
+    // Display the winner:
+    if (humanIsWinner) {
+    playButton.innerText = 'You Win!!!!!';
+    playButton.classList.toggle('winning-text')
+  } else {
+    computerWinsDisplay.innerText = 'Computer Wins!!!';
+  };
+
+    //Disable play button:
+    playButton.setAttribute('disabled', true);
+    nextRoundButton.removeAttribute('disabled');
+});
+
+  
 
 nextRoundButton.addEventListener('click', () => {
     // Increase the round number
@@ -57,10 +70,11 @@ nextRoundButton.addEventListener('click', () => {
   
     // Set the correct disabled state for the buttons
     nextRoundButton.setAttribute('disabled', true);
-    guessButton.removeAttribute('disabled');
+    playButton.removeAttribute('disabled');
 
     //Reset the button displays
     document.getElementById('c_rock').style.display = "none";
     document.getElementById('c_paper').style.display = "none";
     document.getElementById('c_scissors').style.display = "none";
     document.getElementById('c_default').style.display = "block"
+});
